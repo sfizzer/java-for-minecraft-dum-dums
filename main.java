c4faae116ee6ee4eff06cfc5686132cb8303eaf6
juno <140362245+sfizzer@users.noreply.github.com>
2024-08-11 11:18:09 -0500

    Create main.java

---
+++ main.java
@@ -0,0 +1,8 @@
+public class Main {
+    byte aTinyNumber = 100; //one byte (or eight bits) stores nums from [-128 to 127]
+
+    short aSmallNumber = 30000; //short stores nums from [-32,768, to 32,767]
+    
+    long aLongNumber = 50000000; //long (long nums) [-9,223,372,036,854,775,808]// to 9,223,372,036,854,775,807]
+    
+}
